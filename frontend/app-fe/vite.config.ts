0d1fae3d3056fa79f049a94e135daf96e26de684
Getnett <getnetendaylalu2015@gmail.com>
2026-05-17 12:34:26 +0300

    add shadcn radix ui components

--- frontend/app-fe/vite.config.ts
+++ frontend/app-fe/vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path'
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vitest/config'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -6,8 +8,14 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
